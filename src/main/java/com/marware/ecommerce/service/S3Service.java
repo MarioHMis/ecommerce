09503975d522cdfdb364f2cb995cad63809d1858
mariohmis <138615425+MarioHMis@users.noreply.github.com>
2025-07-11 11:25:55 -0600

    feat: add S3Service to handle file uploads using environment variables

--- src/main/java/com/marware/ecommerce/service/S3Service.java
+++ src/main/java/com/marware/ecommerce/service/S3Service.java
@@ -1,9 +1,6 @@
 package com.marware.ecommerce.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
@@ -11,49 +8,38 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
-import java.io.IOException;
-import java.net.URI;
+import java.io.InputStream;
 import java.util.UUID;
 
 @Service
-@RequiredArgsConstructor
 public class S3Service {
 
-    @Value("${aws.s3.bucket}")
-    private String bucket;
+    private final String bucketName = System.getenv("AWS_S3_BUCKET");
+    private final String region = System.getenv("AWS_REGION");
+    private final String accessKey = System.getenv("AWS_ACCESS_KEY");
+    private final String secretKey = System.getenv("AWS_SECRET_KEY");
 
-    @Value("${aws.region}")
-    private String region;
-
-    @Value("${aws.access.key}")
-    private String accessKey;
-
-    @Value("${aws.secret.key}")
-    private String secretKey;
-
-    public String uploadFile(MultipartFile file) {
-        String filename = UUID.randomUUID() + "-" + file.getOriginalFilename();
+    public String uploadFile(InputStream inputStream, long contentLength, String contentType) {
+        String filename = UUID.randomUUID() + ".jpg";
 
         S3Client s3 = S3Client.builder()
                 .region(Region.of(region))
                 .credentialsProvider(
-                        StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey))
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKey, secretKey)
+                        )
                 )
                 .build();
 
-        try {
-            s3.putObject(
-                    PutObjectRequest.builder()
-                            .bucket(bucket)
-                            .key(filename)
-                            .contentType(file.getContentType())
-                            .build(),
-                    RequestBody.fromBytes(file.getBytes())
-            );
-        } catch (IOException e) {
-            throw new RuntimeException("Error uploading file", e);
-        }
+        s3.putObject(
+                PutObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(filename)
+                        .contentType(contentType)
+                        .build(),
+                RequestBody.fromInputStream(inputStream, contentLength)
+        );
 
-        return "https://" + bucket + ".s3." + region + ".amazonaws.com/" + filename;
+        return String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, filename);
     }
 }
